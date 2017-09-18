2d2981ed13f49e7cbe6bb078d3c6cdb4d3628c9a
Aleksander Chromik <achromik@gmail.com>
2017-09-19 01:01:29 +0200

    All ssection. Section COMUNITY w/o RWD

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,92 @@
-$(document).on('click','.navbar-collapse.in',function(event) {
-    if( $(event.target).is('a') ) {
-        $(this).collapse('hide');
-    }
-});
-
 $(function () {
+
+// ******************* COLAPSING MENUBAR **************** */
+
+
+    //collapse menu bar if click on menu item
+    $(document).on('click','.navbar-collapse.in', function(event) {
+        if( $(event.target).is('a') ) {
+            $(this).collapse('hide');
+        }
+    });
+
+
+//******************** SLIDING MENU BAR on scroll ****************** */
+
+    //show or hide nav bar if scroll page
+    var lastScrollTop = 0;
+    $(window).on('scroll', function(event) {
+        var st = $(this).scrollTop();
+        
+        //seting background color of navbar related to scroll from top and to screen width
+        //viewport is 798px or more
+        if ($(this).width() > 797) {
+            //set backgroud-color of navbar when scroll is more than 250px from top
+            if ($(this).scrollTop() > 250) {
+                $('.navbar-nav').css({'background-color': 'rgba(0, 0, 0, .6'});
+            //set transparent background if scroll is less than 250px    
+            } else {
+                $('.navbar-nav').css({'background-color': 'rgba(0, 0, 0, 0'});
+            }
+        //viewport on small screens
+        } else {
+
+            $('.navbar-nav').css({'background-color': 'rgba(0, 0, 0, .6'});            
+            if ($(this).scrollTop() > 50) {
+                $('.navbar-header').css({'background-color': 'rgba(0, 0, 0, .6'});
+            } else {
+                $('.navbar-header').css({'background-color': 'rgba(0, 0, 0, 0'});
+            }
+        }
+
+        //hide navbar when downscroll is more than 25px
+        if (st - lastScrollTop > 25 ){
+            $('.navbar-nav').slideUp(400);
+            $('.navbar-header').slideUp(400);
+            $('.navbar-collapse.in').collapse('hide');
+            lastScrollTop = st;
+        //show navbar when upscroll is more than 25px    
+        } else if (st - lastScrollTop < -25 ){
+            
+            $('.navbar-nav').slideDown(400);
+            $('.navbar-header').slideDown(400);
+            lastScrollTop = st;
+        }
+    }); 
+
     
+// ********************** SMOOTH SCROLL *************************
+
+    $("a").on('click', function(event) {
+
+        //duration of the top scrolling animation (in ms)
+        var scroll_duration = $(document).height() /2.5;
+
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (1200) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, scroll_duration, function(){
+        
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+
+
+
+// **************** SLIDER ***************************************
+
     var CONST = Object.freeze({
         CAROUSEL_INTERVAL: 0,
         CAROUSEL_MOVE_TIME: 700,
